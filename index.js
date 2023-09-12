c537cb153cc4d543108183f922b146c619a9214a
Ahmed Momodu <ahmedmomodu97@gmail.com>
2023-09-12 15:02:12 +0100

    Updated JS script and github repo link (target = _blank)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var x = setInterval(function getUTCTime(){
     const now = new Date();
     const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     
-
+  /* To get the current time in dd:mm:ss:ms format--------------
     const hours = now.getUTCHours();
     const minutes = now.getUTCMinutes();
     const seconds = now.getUTCSeconds();
@@ -12,8 +12,10 @@ var x = setInterval(function getUTCTime(){
 
     var current_day = weekday[now.getDay()];
     var current_time = `${hours}:${minutes}:${seconds}.${milliseconds}`;
-
     document.querySelector("[data-testid='currentUTCTime']").innerHTML = current_time;
+    */
+
+    document.querySelector("[data-testid='currentUTCTime']").innerHTML = now.getTime();
 
     document.querySelector("[data-testid='currentDayOfTheWeek']").innerHTML = current_day;
 }, 1);
